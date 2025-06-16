24fbf54d7caeb711360b2058805ba1add1cd0962
Manav Baldaniya <154751985+manav121257NN@users.noreply.github.com>
2025-06-16 17:52:07 +0530

    Update contractConfig.js

--- crowdfunding-frontend/src/contractConfig.js
+++ crowdfunding-frontend/src/contractConfig.js
@@ -1,5 +1,5 @@
 // contractConfig.js
 import contractABI from "./contractABI.json";
 
-export const contractAddress = "0x95a669B6862EbfA92e7bf3dbb0780dCC5eC52a62"; // Replace with your deployed contract address
+export const contractAddress = "###"; // Replace with your deployed contract address
 export const contractABIArray = contractABI.abi;
